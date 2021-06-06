3c03c3751c8d5da39fc193af422a8b7ae70ad42f
Aditya A <adityaaryan000@gmail.com>
2021-06-06 16:48:39 +0530

    first commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const { response } = require("express");
 
 const app = express();
@@ -8,28 +9,73 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
-let listItems = ["buy food", "cook food", "eat food"];
-app.get("/", function(req, res){
+//connecting to mongoDB database
+mongoose.connect("mongodb://localhost:27017/todoListDB", {useNewUrlParser: true});
 
-    let options = {
-        weekday:"long", 
-        year: "numeric",
-        month:"long",
-        day:"numeric"
-    }
-    let today = new Date();
-    let day = today.toLocaleDateString("en-us", options);
-    res.render("list.ejs", {kindofDay: day, addItems: listItems});
-   
+//todoList schema 
+const listSchema = new mongoose.Schema({
+    name: String
+});
+//model created
+const Item = mongoose.model("Item", listSchema);
+
+const newItem = new Item({
+    name: "WElcome "
+});
+const newItem1 = new Item({
+    name: "To your list"
+});
+
+const newItem2 = new Item({
+    name: "hit tthat add "
+});
+
+const defaultItems = [newItem, newItem1, newItem2]
+
+Item.insertMany(defaultItems, function(err){
+    if(err){
+        console.log(err);
+    }else console.log("successfully inserted");
 })
+
+app.get("/", function(req, res){
+    res.render("list.ejs", {
+        listTitle:"Today",
+        addItems: defaultItems
+    })
+});
+
+app.get("/about", function(req, res){
+    res.render("about.ejs");
+});
 
 app.post("/", function(req, res){
-  
+    
+
     let item = req.body.newItem;
-    listItems.push(item);
+    
+    if(req.body.list === "Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    }
+    else{
+        
+    console.log(item);
+    defaultItems.push(item);
     res.redirect("/");
+    }
+})
+app.get("/work", function(req, res){
+    
+    res.render("list", {listTitle: "Work", addItems: workItems})
 })
 
+app.post("/work", function(req, res){
+    let item = req.body.newItem;
+    console.log("got this in work list ");
+    workItems.push(item);
+    res.redirect("/work");
+});
 
 app.listen(3030, function(){
     console.log("Listening at port 3030");
